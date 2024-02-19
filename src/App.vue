6be9f041349eecc1135ac532485a5c9f4812f4c6
yusef02 <youssef.bouziane2402@gmail.com>
2024-02-19 20:30:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: { AppHeader, AppMain, AppFooter },
+};
+</script>
+<template>
+  <app-header />
+  <app-main />
+  <app-footer />
+</template>
+<style lang="scss"></style>
