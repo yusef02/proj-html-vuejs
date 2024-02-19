d9cefb35168d75ff8a14a928933d10c93f53a658
yusef02 <youssef.bouziane2402@gmail.com>
2024-02-20 00:22:49 +0100

    script(appHero): aggiunto componente hero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <section id="hero">
+    <div class="container">
+      <h1>titolo</h1>
+      <p>descrizione</p>
+      <div class="btn">scarica</div>
+    </div>
+    <section id="heroMotto">
+      <div class="container">motto brief brief brief</div>
+    </section>
+  </section>
+</template>
+<style lang="scss"></style>
---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+  logo: "logo.png",
+});
