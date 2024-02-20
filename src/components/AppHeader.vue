c1b4aab2db9d54ce2d36dee5fb7cfa9a31536d6e
yusef02 <youssef.bouziane2402@gmail.com>
2024-02-20 13:45:42 +0100

    style(header): aggiunto un minimo di stile componente header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -37,12 +37,38 @@ export default {
 };
 </script>
 <template>
-  <header>
+  <header id="header">
     <div class="container">
-      <app-logo />
-      <app-nav :navLinks="navLinks" />
-      <div class="btn">JOIN US</div>
+      <div class="header__block">
+        <app-logo />
+      </div>
+      <div class="header__block">
+        <app-nav :navLinks="navLinks" />
+        <div class="btn">JOIN US</div>
+      </div>
     </div>
   </header>
 </template>
-<style lang="scss"></style>
+<style lang="scss">
+#header {
+  color: white;
+  background-color: #28afc3;
+  padding: 20px 0;
+  .container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    .header__block {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: 30px;
+      .btn {
+        color: white;
+        background-color: rgb(19, 19, 19);
+      }
+    }
+  }
+}
+</style>
--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -7,12 +7,28 @@ export default {
 };
 </script>
 <template>
-  <div>
-    <ul>
-      <li v-for="tag in navLinks">
-        <a :href="tag.tag">{{ tag.tagText }}</a>
-      </li>
-    </ul>
-  </div>
+  <ul class="navList">
+    <li v-for="tag in navLinks">
+      <a :href="tag.tag">{{ tag.tagText }}</a>
+    </li>
+  </ul>
 </template>
-<style lang="scss"></style>
+<style lang="scss">
+.navList {
+  list-style: none;
+  padding-left: 0;
+  margin-bottom: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  gap: 10px;
+  li {
+    padding: 10px;
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+}
+</style>
